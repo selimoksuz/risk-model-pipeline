4a7d1b4676f4f5bd040171c591590813008d1575
selim oksuz <s.oksuzz61@gmail.com>
2025-09-24 20:43:03 +0300

    chore: bump package version to 0.4.0

--- src/risk_pipeline/_version.py
+++ src/risk_pipeline/_version.py
@@ -1,4 +1,1 @@
-"""Version information for risk-model-pipeline package"""
-
-__version__ = "0.3.0"
-__version_info__ = tuple(int(i) for i in __version__.split("."))
+"""Version information for risk-model-pipeline package"""\n\n__version__ = "0.4.0"\n__version_info__ = tuple(int(i) for i in __version__.split("."))\n
